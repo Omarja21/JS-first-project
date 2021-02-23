9aab916c8c5c1bb5e94f3a7987264b16c300b741
Omarja21 <79086495+Omarja21@users.noreply.github.com>
2021-02-23 20:44:33 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,12 @@
+var Auditions=prompt("rate my performance from 1 to 10")
+
+if (Auditions < 5){
+    alert('Bad boy go and rate me higher');
+}
+else if (Auditions >= 5 && Auditions < 8){
+    alert("not bad boy");
+}
+else if(Auditions > 8){
+    alert("Good booooy here is a cookie for U")
+}
+
